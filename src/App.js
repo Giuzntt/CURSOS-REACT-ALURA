5006364d309b47ff42eb91ef2c40dfc87c063947
giuzntt <giuzntt@gmail.com>
2021-12-10 15:56:47 -0300

    ,,

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component, Fragment } from 'react';
-
+import React, { Component } from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Poppins from "@fontsource/poppins";
 
 class App  extends Component{
 
-      
+    
+
   render(){
 
     
     return (
-      <Fragment>
-        <h1>Aqui o meu formulario</h1>
-      <FormularioCadastro />
-      </Fragment>
+      <Container component="article" maxWidth="lg">
+        <Typography className='atata' variant="h5" color="initial" textAlign='center'>Preencha o seu formulário</Typography>
+        <FormularioCadastro />
+      </Container>
+     
+     
       );
     }
 }
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,35 +1,58 @@
 import React from 'react';
 import Button from '@material-ui/core/Button'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
+import TextField from '@material-ui/core/TextField'
+import {  FormControlLabel, FormGroup, Switch } from '@material-ui/core';
 
 
 
 function FormularioCadastro() {
-
+    let nome ="";
     return (
-        
-           
-            <form>
-                <label>Nome</label>
-                <input type="text" />
-                <label>Sobrenome</label>
-                <input type="text" />
-                <label>CPF</label>
-                <input type="text" />
-                <label>Promocões</label>
-                <input type="checkbox" />
-                <label>Novidades</label>
-                <input type="checkbox" />
+    <form onSubmit={(event=>{
+        event.preventDefault();
+        console.log(nome);
+    })}>
+        <TextField
+            value={nome}
+            onChange={(event) =>{
+                    nome = event.target.value
+                    if (nome.length > 3) {
+                        nome = nome.substr(0,3)
+                    }
 
-               <Button variant="contained" color="secondary" >
-                 Cadastrar
-               </Button>
-               
-               
-            </form>
-)
+            }}
+                id="outlined-basic"
+                label="Nome" variant="outlined"
+                fullWidth margin='normal'
+            />
+            <TextField id="outlined-basic" label="Sobrenome" variant="outlined" fullWidth margin='normal' />
+            <TextField id="outlined-basic" label="CPF" variant="outlined" fullWidth margin='normal' />
 
-    
+         
+
+            <FormGroup>
+                <FormControlLabel control={<Switch defaultChecked  color='primary'/>} label="Promocoes" />
+                <FormControlLabel control={<Switch defaultChecked color='primary'/>} label="Novidades" />
+            </FormGroup>
+         
+
+
+            
+
+            < Button variant="contained"
+                type='submit'
+                color="primary"
+                fullWidth >
+                Cadastrar
+                
+            </Button>
+                   
+
+
+        </form>
+    )
+
+
 }
 
 
